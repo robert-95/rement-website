66b25f2510190e7c87dc8cd65712af6efc627d6f
Stackbit Code Editor <projects@stackbit.com>
2023-04-11 19:11:07 +0000

    stackbit.config.ts: changed

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -22,7 +22,7 @@ export const config = defineStackbitConfig({
     pageLayoutKey: 'type',
     styleObjectModelName: 'ThemeStyle'
 });
-export default config;
+// export default config;
 
 export default {
   models: {
