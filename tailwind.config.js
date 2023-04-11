52cb49d685aec96d0ce609371ff7a5511727cfd8
Stackbit Code Editor <projects@stackbit.com>
2023-04-11 18:48:54 +0000

    tailwind.config.js: changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
         extend: {
             fontFamily: {
                 sans: ['roboto-condensed-regular', sans],
-                serif: ['staatliches-regular']
+                serif: ['staatliches-regular', sans-serif]
             },            
             boxShadow: {
                 header: '0px 2px 8px rgba(27, 32, 50, .08)'
