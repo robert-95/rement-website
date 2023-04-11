4f16931c105fc7cdb79916f8e29531df3d9c14f7
Stackbit Code Editor <projects@stackbit.com>
2023-04-11 18:24:40 +0000

    tailwind.config.js: changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,8 +23,8 @@ module.exports = {
                 primary: themeStyle.primary
             },
             fontFamily: {
-                sans: ['roboto-condensed'],
-                serif: ['Staatliches']
+                sans: ['roboto-condensed-regular'],
+                serif: ['staatliches']
             },
             gridTemplateColumns: {
                 16: 'repeat(16, minmax(0, 1fr))'
