472b36b49286e924cfd76db7e7d1dc14d02d713c
Stackbit Code Editor <projects@stackbit.com>
2023-04-16 20:28:12 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -56,7 +56,7 @@ function HeaderVariants(props) {
 function HeaderLogoLeftPrimaryLeft(props) {
     const { title, logo, primaryLinks = [], secondaryLinks = [], colors = 'bg-light-fg-dark' } = props;
     return (
-        <div className="flex items-center relative max-h-6 ">
+        <div className="flex items-center relative max-h-6 overflow-hidden">
             {(title || logo?.url) && (
                 <div className="mr-10">
                     <SiteLogoLink title={title} logo={logo} />
