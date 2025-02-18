796970003dbb2fe8347549b397a0497c4534504d
robert-95 <58114030+robert-95@users.noreply.github.com>
2025-02-18 13:10:39 +0100

    Update index.tsx

--- src/components/layouts/DefaultBaseLayout/index.tsx
+++ src/components/layouts/DefaultBaseLayout/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Head from 'next/head';
 import classNames from 'classnames';
 import Header from '../../sections/Header';
+import SecondaryHeader from '../../sections/SecondaryHeader';  // Importiere den SecondaryHeader
 import Footer from '../../sections/Footer';
 import { seoGenerateTitle, seoGenerateMetaTags, seoGenerateMetaDescription } from '../../../utils/seo-utils';
 
@@ -11,6 +12,10 @@ export default function DefaultBaseLayout(props) {
     let title = seoGenerateTitle(page, site);
     let metaTags = seoGenerateMetaTags(page, site);
     let metaDescription = seoGenerateMetaDescription(page, site);
+    
+    // Den SecondaryHeader aus der Page (oder Site) extrahieren
+    const { secondaryHeader } = page || {};
+
     return (
         <div className={classNames('sb-page', pageMeta.pageCssClasses)} data-sb-object-id={pageMeta.id}>
             <div className="sb-base sb-default-base-layout">
@@ -19,7 +24,7 @@ export default function DefaultBaseLayout(props) {
                     {metaDescription && <meta name="description" content={metaDescription} />}
                     {metaTags.map((metaTag) => {
                         if (metaTag.format === 'property') {
-                            // OpenGraph meta tags (og:*) should be have the format <meta property="og:…" content="…">
+                            // OpenGraph meta tags (og:*) sollten das Format <meta property="og:…" content="…"> haben
                             return <meta key={metaTag.property} property={metaTag.property} content={metaTag.content} />;
                         }
                         return <meta key={metaTag.property} name={metaTag.property} content={metaTag.content} />;
@@ -28,8 +33,16 @@ export default function DefaultBaseLayout(props) {
                     <meta name="google-site-verification" content="QxO03R5kblCWRLwf0Nz23ZLZtymP2z3y8aVPa9HE_fE" />
                     {site.favicon && <link rel="icon" href={site.favicon} />}
                 </Head>
-                {site.header && <Header {...site.header} data-sb-object-id={site.header?.__metadata?.id} />}
+
+                {/* Hier Header oder SecondaryHeader abhängig vom Vorhandensein rendern */}
+                {secondaryHeader ? (
+                    <SecondaryHeader {...secondaryHeader} data-sb-object-id={secondaryHeader?.__metadata?.id} />
+                ) : (
+                    site.header && <Header {...site.header} data-sb-object-id={site.header?.__metadata?.id} />
+                )}
+
                 {props.children}
+
                 {site.footer && <Footer {...site.footer} data-sb-object-id={site.footer?.__metadata?.id} />}
             </div>
         </div>
