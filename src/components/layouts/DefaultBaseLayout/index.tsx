c668d50ed28a5ff7448f9fe12903f4c08d8baa79
robert-95 <58114030+robert-95@users.noreply.github.com>
2025-02-18 16:38:30 +0100

    Update index.tsx

--- src/components/layouts/DefaultBaseLayout/index.tsx
+++ src/components/layouts/DefaultBaseLayout/index.tsx
@@ -4,7 +4,6 @@ import classNames from 'classnames';
 import Header from '../../sections/Header';
 import Footer from '../../sections/Footer';
 import { seoGenerateTitle, seoGenerateMetaTags, seoGenerateMetaDescription } from '../../../utils/seo-utils';
-import siteConfig from '../../../../content/data/site.json';  // site.json importieren
 
 export default function DefaultBaseLayout(props) {
     const { page, site } = props;
@@ -12,10 +11,8 @@ export default function DefaultBaseLayout(props) {
     let title = seoGenerateTitle(page, site);
     let metaTags = seoGenerateMetaTags(page, site);
     let metaDescription = seoGenerateMetaDescription(page, site);
-
-    // Überprüfen, ob `page.english` true ist und entsprechend den Header auswählen
-    const headerKey = page.english ? 'header-english' : 'header';
-    const headerData = siteConfig[headerKey];  // Dynamisch header-english oder header wählen
+    const isEnglish = page?.english === true;
+    const headerData = isEnglish ? site['header-english'] : site.header;
 
     return (
         <div className={classNames('sb-page', pageMeta.pageCssClasses)} data-sb-object-id={pageMeta.id}>
@@ -25,6 +22,7 @@ export default function DefaultBaseLayout(props) {
                     {metaDescription && <meta name="description" content={metaDescription} />}
                     {metaTags.map((metaTag) => {
                         if (metaTag.format === 'property') {
+                            // OpenGraph meta tags (og:*) should be have the format <meta property="og:…" content="…">
                             return <meta key={metaTag.property} property={metaTag.property} content={metaTag.content} />;
                         }
                         return <meta key={metaTag.property} name={metaTag.property} content={metaTag.content} />;
@@ -33,12 +31,8 @@ export default function DefaultBaseLayout(props) {
                     <meta name="google-site-verification" content="QxO03R5kblCWRLwf0Nz23ZLZtymP2z3y8aVPa9HE_fE" />
                     {site.favicon && <link rel="icon" href={site.favicon} />}
                 </Head>
-                
-                {/* Header basierend auf der Bedingung laden */}
                 {headerData && <Header {...headerData} data-sb-object-id={headerData?.__metadata?.id} />}
-                
                 {props.children}
-                
                 {site.footer && <Footer {...site.footer} data-sb-object-id={site.footer?.__metadata?.id} />}
             </div>
         </div>
