0d1ebd45a1549179df70f5926be3ae79bf039312
robert-95 <58114030+robert-95@users.noreply.github.com>
2025-02-18 13:20:06 +0100

    Update index.tsx

--- src/components/layouts/DefaultBaseLayout/index.tsx
+++ src/components/layouts/DefaultBaseLayout/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Head from 'next/head';
 import classNames from 'classnames';
 import Header from '../../sections/Header';
-import SecondaryHeader from '../../sections/SecondaryHeader';  // Importiere den SecondaryHeader
+import SecondaryHeader from '../../sections/secondaryHeader';  // Importiere den SecondaryHeader
 import Footer from '../../sections/Footer';
 import { seoGenerateTitle, seoGenerateMetaTags, seoGenerateMetaDescription } from '../../../utils/seo-utils';
 
