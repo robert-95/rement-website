66f3e256cdf8112d0c47a7cd8d56c9707ede7a2a
robert-95 <58114030+robert-95@users.noreply.github.com>
2025-02-20 09:29:38 +0100

    Update components-registry.ts

--- src/components/components-registry.ts
+++ src/components/components-registry.ts
@@ -1,36 +1,16 @@
 import dynamic from 'next/dynamic';
 import { ComponentType } from 'react';
-import withPersonalization from './withPersonalization';
 
 /**
  * The getComponent() function loads a component using dynamic import.
- *
- * Dynamic imports are useful when you wish to load a module conditionally. For example, if a home page renders the
- * "HeroSection" conditionally, then loading it with getComponent('HeroSection') will ensure that the "HeroSection"
- * is bundled only when used.
  */
 
 export function getComponent(key: string): ComponentType {
-    const component = components[key];
-    return personalizedComponents[key] ? withPersonalization(component, personalizedComponents[key]) : component;
+    return components[key]; // No personalization applied
 }
 
 /**
  * Map of dynamically imported components.
- *
- * The mapping key of a dynamically imported component is the model name describing the props of that component.
- * And the value is the component imported via Next.js dynamic import. You should use dynamic components for large
- * components or components with heavy external dependencies that are used sparingly in your website's pages.
- * To learn more about Nextjs dynamic imports visit:
- * https://nextjs.org/docs/advanced-features/dynamic-import
- *
- * Dynamic components can be selected at run-time based on the type of their content (props). This is because
- * components are mapped by models that describe their content, and content's type always matches the model name.
- * For example, a page component can call `getComponent(section.__metadata.modelName)` function, passing it the type of section
- * data it needs to render, and get back the component that can render that type of data:
- *
- *     const Section = getComponent(section.__metadata.modelName);
- *     return <Section {...section} />;
  */
 const components = {
     AutoCompletePosts: dynamic(() => import('./blocks/SearchBlock/AutoCompletePosts')),
@@ -58,12 +38,4 @@ const components = {
     PostLayout: dynamic(() => import('./layouts/PostLayout')),
     PostFeedLayout: dynamic(() => import('./layouts/PostFeedLayout')),
     PostFeedCategoryLayout: dynamic(() => import('./layouts/PostFeedCategoryLayout'))
-};
-
-/**
- * For any component configured as personalizable (via Ninetailed's Contentful App),
- * add the model name as key and the "label field" as value (this is used in the variant selection dropdown).
- */
-const personalizedComponents = {
-    GenericSection: 'subtitle'
 };
