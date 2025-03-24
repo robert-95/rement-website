95c7310e7122296cd306aa0f22c0f21a4c865215
robert-95 <58114030+robert-95@users.noreply.github.com>
2025-03-24 09:34:57 +0100

    Add files via upload

---
+++ next-sitemap.config.js
@@ -0,0 +1,19 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2821
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red87\green96\blue106;\red201\green26\blue51;\red0\green0\blue0;
+\red27\green31\blue34;\red7\green68\blue184;\red6\green33\blue79;}
+{\*\expandedcolortbl;;\cssrgb\c41569\c45098\c49020;\cssrgb\c83529\c18824\c25882;\cssrgb\c0\c0\c0;
+\cssrgb\c14118\c16078\c18039;\cssrgb\c0\c36078\c77255;\cssrgb\c1176\c18431\c38431;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs32 \cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 /** \cf3 \strokec3 @type\cf2 \strokec2  \{import('next-sitemap').IConfig\} */\cf0 \strokec4 \
+\pard\pardeftab720\partightenfactor0
+\cf5 \strokec5 module\cf0 \strokec4 .\cf6 \strokec6 exports\cf0 \strokec4  \cf6 \strokec6 =\cf0 \strokec4  \{\
+  \cf6 \strokec6 siteUrl\cf0 \strokec4 : process.\cf6 \strokec6 env\cf0 \strokec4 .\cf6 \strokec6 SITE_URL\cf0 \strokec4  \cf6 \strokec6 ||\cf0 \strokec4  \cf7 \strokec7 'https://rement.tech\cf0 \strokec4 ,\
+  \cf6 \strokec6 generateRobotsTxt\cf0 \strokec4 : \cf6 \strokec6 true\cf0 \strokec4 , \cf2 \strokec2 // (optional)\cf0 \strokec4 \
+  \cf2 \strokec2 // ...other options\cf0 \strokec4 \
+\}\
+}
