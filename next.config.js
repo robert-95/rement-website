757e5b394fafe2192f094c95756f0b02b258357d
robert-95 <58114030+robert-95@users.noreply.github.com>
2025-07-23 14:00:43 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,14 +2,28 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    env: {
-        stackbitPreview: process.env.STACKBIT_PREVIEW
-    },
-    trailingSlash: true,
-    webpack: (config) => {
-        config.watchOptions.ignored.push('**/content/pages/**');
-        return config;
+  env: {
+    stackbitPreview: process.env.STACKBIT_PREVIEW || ''
+  },
+  trailingSlash: true,
+  webpack: (config) => {
+    if (!config.watchOptions) {
+      config.watchOptions = {};
     }
+
+    if (!config.watchOptions.ignored) {
+      config.watchOptions.ignored = [];
+    } else if (typeof config.watchOptions.ignored === 'string') {
+      config.watchOptions.ignored = [config.watchOptions.ignored];
+    } else if (!Array.isArray(config.watchOptions.ignored)) {
+      // Just in case it's something else, make it an array
+      config.watchOptions.ignored = [];
+    }
+
+    config.watchOptions.ignored.push('**/content/pages/**');
+
+    return config;
+  }
 };
 
 module.exports = nextConfig;
