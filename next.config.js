b23550a9483644087167980c94f3ad572db0cc1d
robert-95 <58114030+robert-95@users.noreply.github.com>
2025-07-23 14:03:45 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,28 +2,25 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  env: {
-    stackbitPreview: process.env.STACKBIT_PREVIEW || ''
-  },
-  trailingSlash: true,
-  webpack: (config) => {
-    if (!config.watchOptions) {
-      config.watchOptions = {};
+    env: {
+        stackbitPreview: process.env.STACKBIT_PREVIEW
+    },
+    trailingSlash: true,
+    webpack: (config) => {
+        if (!config.watchOptions) {
+            config.watchOptions = {};
+        }
+
+        if (!config.watchOptions.ignored) {
+            config.watchOptions.ignored = [];
+        } else if (typeof config.watchOptions.ignored === 'string') {
+            config.watchOptions.ignored = [config.watchOptions.ignored];
+        }
+
+        config.watchOptions.ignored.push('**/content/pages/**');
+
+        return config;
     }
-
-    if (!config.watchOptions.ignored) {
-      config.watchOptions.ignored = [];
-    } else if (typeof config.watchOptions.ignored === 'string') {
-      config.watchOptions.ignored = [config.watchOptions.ignored];
-    } else if (!Array.isArray(config.watchOptions.ignored)) {
-      // Just in case it's something else, make it an array
-      config.watchOptions.ignored = [];
-    }
-
-    config.watchOptions.ignored.push('**/content/pages/**');
-
-    return config;
-  }
 };
 
 module.exports = nextConfig;
